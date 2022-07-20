6b8dd37dc3fbd977db02ead6b3f8ae98c73ee7e4
Ajay Kumar Rajamundry <Ajay.Kumar.Rajamundry@cybercom.com>
2022-07-20 22:15:38 +0200

    Improving styles and enhancing date

--- Aj/basic-react-app/src/App.js
+++ Aj/basic-react-app/src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
 import ExpenseItem from './components/ExpenseItem/ExpenseItem';
-
+import expensesData from './components/ExpenseItem/ExpensesData'
 function App() {
     return (
-        <ExpenseItem />
+        <>
+            <ExpenseItem
+                title={expensesData[0].title}
+                amount={expensesData[0].amount}
+                date={expensesData[0].date} />
+            <ExpenseItem
+                title={expensesData[1].title}
+                amount={expensesData[1].amount}
+                date={expensesData[1].date} />
+            <ExpenseItem
+                title={expensesData[2].title}
+                amount={expensesData[2].amount}
+                date={expensesData[2].date} />
+            <ExpenseItem
+                title={expensesData[3].title}
+                amount={expensesData[3].amount}
+                date={expensesData[3].date} />
+        </>
+
     );
 }
 
--- Aj/basic-react-app/src/components/ExpenseItem/ExpenseItem.js
+++ Aj/basic-react-app/src/components/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,22 @@
 import './ExpenseItem.css'
-function ExpenseItem() {
-    const expenseDate = new Date(2021, 2, 28);
-    const expenseTitle = "Car Insurance";
-    const expenseAmount = 294.64;
+function ExpenseItem(props) {
+    // Date
+    const month = props.date.toLocaleString('en-US', { month: 'long' })
+    const day = props.date.toLocaleString('en-US', { day: '2-digit' })
+    const year = props.date.getFullYear()
 
-
+    const expenseTitle = props.title;
+    const expenseAmount = props.amount;
     return (
         <div className="expense-item">
-            <div>{expenseDate.toDateString()}</div>
-            <div className="expense-item__description">
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
+            <div className="expense-item-description">
                 <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <div className="expense-item-price">${expenseAmount}</div>
             </div>
         </div>
     )
