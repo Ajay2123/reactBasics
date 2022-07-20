3bedea7930580e6f50fc054e3d2ff62d3767942c
Ajay Kumar Rajamundry <Ajay.Kumar.Rajamundry@cybercom.com>
2022-07-20 22:24:13 +0200

    Working with nested components

--- Aj/basic-react-app/src/components/ExpenseItem/ExpenseItem.js
+++ Aj/basic-react-app/src/components/ExpenseItem/ExpenseItem.js
@@ -1,19 +1,13 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate'
 function ExpenseItem(props) {
-    // Date
-    const month = props.date.toLocaleString('en-US', { month: 'long' })
-    const day = props.date.toLocaleString('en-US', { day: '2-digit' })
-    const year = props.date.getFullYear()
+
 
     const expenseTitle = props.title;
     const expenseAmount = props.amount;
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{day}</div>
-                <div>{year}</div>
-            </div>
+            <ExpenseDate date={props.date} />
             <div className="expense-item-description">
                 <h2>{expenseTitle}</h2>
                 <div className="expense-item-price">${expenseAmount}</div>
