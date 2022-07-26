a754faaecb886a632a241248293ec45939dc5f21
Ajay Kumar Rajamundry <Ajay.Kumar.Rajamundry@cybercom.com>
2022-07-26 20:50:26 +0200

    Adding expenses styles

--- Aj/basic-react-app/src/App.js
+++ Aj/basic-react-app/src/App.js
@@ -1,25 +1,10 @@
 import './App.css';
-import ExpenseItem from './components/ExpenseItem/ExpenseItem';
 import expenseData from './components/ExpenseItem/ExpenseData';
+import Expenses from './components/Expenses/Expenses'
 function App() {
     return (
         <>
-            <ExpenseItem
-                title={expenseData[0].title}
-                amount={expenseData[0].amount}
-                date={expenseData[0].date} />
-            <ExpenseItem
-                title={expenseData[1].title}
-                amount={expenseData[1].amount}
-                date={expenseData[1].date} />
-            <ExpenseItem
-                title={expenseData[2].title}
-                amount={expenseData[2].amount}
-                date={expenseData[2].date} />
-            <ExpenseItem
-                title={expenseData[3].title}
-                amount={expenseData[3].amount}
-                date={expenseData[3].date} />
+            <Expenses data={expenseData} />
         </>
 
     );
