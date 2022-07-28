e81040bc178e0a0e20438a9162ceb6c277c64c10
Ajay Kumar Rajamundry <Ajay.Kumar.Rajamundry@cybercom.com>
2022-07-28 21:52:44 +0200

    Implementing Use state

--- Aj/basic-react-app/src/components/Expenses/ExpenseItem.js
+++ Aj/basic-react-app/src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,20 @@
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate'
 import './ExpenseItem.css'
 import Card from '../Card/Card';
 
-function ExpenseItem(props) {
-    const expenseTitle = props.title;
-    const expenseAmount = props.amount;
+const ExpenseItem = (props) => {
+    let [amount, setAmount] = useState(props.amount);
+    const clickHandler = () => setAmount(amount += 1);
+
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item-description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item-price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item-price">${amount}</div>
             </div>
+            <button onClick={clickHandler} >Change Title</button>
         </Card>
     )
 }
