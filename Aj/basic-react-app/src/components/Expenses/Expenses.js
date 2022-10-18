2458d62cbe701b3dc42ffe85921aa1175cd61af1
Ajay2123 <ajayraj2187@gmail.com>
2022-10-18 21:59:26 +0200

    Assignment with passing properties - from child to parent and vice versa

--- Aj/basic-react-app/src/components/Expenses/Expenses.js
+++ Aj/basic-react-app/src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
 import ExpenseItem from './ExpenseItem';
+import ExpensesFilter from './ExpensesFilter';
 import Card from '../Card/Card';
 import "./Expenses.css";
+import { useState } from 'react';
 
 function Expenses(props) {
     const expenseData = props.data;
+    const [selectedYear, setSelectedYear] = useState("2020");
+
+    const filterByYearHandler = (year) => {
+        setSelectedYear(_ => year);
+        console.log("Filtering by year :", year)
+    }
     return <Card className="expenses">
+        <ExpensesFilter selected={selectedYear} onChangeFilter={filterByYearHandler} ></ExpensesFilter>
         <ExpenseItem
             title={expenseData[0].title}
             amount={expenseData[0].amount}
