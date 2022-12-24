99fcd53053bafbd19ea58a38df4def04d6716048
Ajay2123 <ajayraj2187@gmail.com>
2022-12-24 11:48:16 +0100

    Showing list dynamiclly, Setting usestate for expense data

--- Aj/basic-react-app/src/App.js
+++ Aj/basic-react-app/src/App.js
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import expenseData from './components/Expenses/ExpenseData';
 import Expenses from './components/Expenses/Expenses'
 import NewExpense from './components/NewExpense/NewExpense';
 
 function App() {
+    const [data, setData] = useState(expenseData);
     const onExpenseAddedHandler = (event) => {
         console.log("Event from app.js ", event)
+        setData(x => [...x, event])
     }
+
     return (
         <>
             <NewExpense onExpenseAdded={onExpenseAddedHandler}></NewExpense>
-            <Expenses data={expenseData} />
+            <Expenses data={data} />
         </>
 
     );
--- Aj/basic-react-app/src/components/Expenses/Expenses.js
+++ Aj/basic-react-app/src/components/Expenses/Expenses.js
@@ -14,22 +14,14 @@ function Expenses(props) {
     }
     return <Card className="expenses">
         <ExpensesFilter selected={selectedYear} onChangeFilter={filterByYearHandler} ></ExpensesFilter>
-        <ExpenseItem
-            title={expenseData[0].title}
-            amount={expenseData[0].amount}
-            date={expenseData[0].date} />
-        <ExpenseItem
-            title={expenseData[1].title}
-            amount={expenseData[1].amount}
-            date={expenseData[1].date} />
-        <ExpenseItem
-            title={expenseData[2].title}
-            amount={expenseData[2].amount}
-            date={expenseData[2].date} />
-        <ExpenseItem
-            title={expenseData[3].title}
-            amount={expenseData[3].amount}
-            date={expenseData[3].date} />
+
+        {
+            props.data.map(item =>
+                <ExpenseItem
+                    title={item.title}
+                    amount={item.amount}
+                    date={item.date} />)
+        }
     </Card>
 }
 
